87d6d0e48d37e978f87bef65ff0f725a3f4e7fb4
juspears <juspears@paypal.com>
2016-04-03 19:43:49 -0700

    Added test fixed fix

--- test/util-test.js
+++ test/util-test.js
@@ -1,4 +1,16 @@
-import expect from 'expect';
+import {onlyKeys} from "../src/util";
+import expect from "expect";
+
 describe('util', function () {
+
+    it('should return only unique keys', function () {
+
+        const ret = onlyKeys(['stuff', 'me'], {stuff: 1, other: 2}, {more: 3}, {me: 4});
+
+        expect(ret.stuff).toBe(1);
+        expect(ret.me).toBe(4);
+        expect(Object.keys(ret).length).toBe(2);
+
+    });
 
 });
